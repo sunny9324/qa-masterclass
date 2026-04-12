c58e5b29ca72c969b614a214947ef0f9906a4631
sunny9324 <46618459+sunny9324@users.noreply.github.com>
2026-04-12 20:29:01 +1000

    update
    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/qa-masterclass/', // <-- This tells the browser exactly which aisle to look in!
 })
